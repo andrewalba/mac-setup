3ba30a0d10ec350b39234d2e24deea192779668e
Andrew Alba <andrew.alba@gmail.com>
2023-11-09 14:07:12 -0600

    🔧 refactor(install_apps.sh): use arch -arm64 prefix for brew install commands to ensure compatibility with Apple Silicon ✨ feat(install_apps.sh): add new apps to brew and AppStore install lists to meet new software requirements

--- install_apps.sh
+++ install_apps.sh
@@ -17,41 +17,57 @@ printf "Installing xcode cli utils\n"
 xcode-select --install
 
 printf "brew: Installing cli packages\n"
-brew install awscli
-brew install azure-cli
-brew install ca-certificates
-brew install git
-brew install node           # NodeJS dev
-brew install n
-brew install openssl        # Generate certificates
-brew install starship
-brew install vim            # Guilty pleasure
-brew install watch
-brew install wget
+arch -arm4 brew install awscli
+arch -arm4 brew install azure-cli
+arch -arm4 brew install ca-certificates
+arch -arm4 brew install git
+arch -arm4 brew install node           # NodeJS dev
+arch -arm4 brew install n
+arch -arm4 brew install openssl        # Generate certificates
+arch -arm4 brew install starship
+arch -arm4 brew install vim            # Guilty pleasure
+arch -arm4 brew install watch
+arch -arm4 brew install wget
 
 # Install ZSH and Oh My ZSH
-brew install zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting
+arch --arm64 brew install zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting
 
 chsh -s /usr/local/bin/zsh
 /usr/local/bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 printf "brew: Installing apps\n"
-brew install --cask firefox
-brew install --cask google-chrome
-brew install --cask grammarly
-brew install --cask handbrake
-brew install --cask iterm2
-brew install --cask java
-brew install --cask ngrok
-brew install --cask postman
-brew install --cask powershell
-brew install --cask raycast
-brew install --cask sublime-text          # Look at purchasing for devs
-brew install --cask visual-studio-code
-brew install --cask vlc
-brew install --cask zoomus
-brew install --cask zoomus-outlook-plugin
+arch -arm64 brew install --cask espanso
+arch -arm64 brew install --cask firefox
+arch -arm64 brew install --cask folx
+arch -arm64 brew install --cask google-chrome
+arch -arm64 brew install --cask grammarly
+arch -arm64 brew install --cask handbrake
+arch -arm64 brew install --cask iterm2
+arch -arm64 brew install --cask java
+arch -arm64 brew install --cask jetbrains-toolbox
+arch -arm64 brew install --cask microsoft-edge
+arch -arm64 brew install --cask microsoft-teams
+arch -arm64 brew install --cask mjml
+arch -arm64 brew install --cask ngrok
+arch -arm64 brew install --cask nomad
+arch -arm64 brew install --cask obs
+arch -arm64 brew install --cask postman
+arch -arm64 brew install --cask powershell
+arch -arm64 brew install --cask r
+arch -arm64 brew install --cask raycast
+arch -arm64 brew install --cask rstudio
+arch -arm64 brew install --cask skype
+arch -arm64 brew install --cask sourcetree
+arch -arm64 brew install --cask subler
+arch -arm64 brew install --cask sublime-text          # Look at purchasing for devs
+arch -arm64 brew install --cask unclutter
+arch -arm64 brew install --cask visual-studio-code
+arch -arm64 brew install --cask vlc
+arch -arm64 brew install --cask yed
+arch -arm64 brew install --cask zoom
+arch -arm64 brew install --cask zoomus
+arch -arm64 brew install --cask zoomus-outlook-plugin
 
 
 # Installs App Store software.
@@ -66,29 +82,44 @@ fi
 printf "AppStore: Installing Bitwarden\n"
 mas install 1352778147
 
-printf "AppStore: Installing The Unarchiver\n"
-mas install 425424353
+printf "AppStore: Installing iMovie\n"
+mas install 408981434
+
+printf "AppStore: Installing Keynote\n"
+mas install 409183694
 
 printf "AppStore: Installing Magnet\n"
 mas install 441258766
 
-printf "AppStore: Installing Microsoft OneDrive\n"
-mas install 823766827
-
-printf "AppStore: Installing Microsoft OneNote\n"
-mas install 784801555
+printf "AppStore: Installing Microsoft Word\n"
+mas install 462054704
 
 printf "AppStore: Installing Microsoft Excel\n"
 mas install 462058435
 
+printf "AppStore: Installing Microsoft PowerPoint\n"
+mas install 462062816
+
+printf "AppStore: Installing Microsoft OneNote\n"
+mas install 784801555
+
+printf "AppStore: Installing Numbers\n"
+mas install 409203825
+
+printf "AppStore: Installing OneDrive\n"
+mas install 823766827
+
 printf "AppStore: Installing Microsoft Outlook\n"
 mas install 985367838
 
-printf "AppStore: Installing Microsoft Word\n"
-mas install 462054704
+printf "AppStore: Installing Pages\n"
+mas install 409201541
 
-printf "AppStore: Installing Microsoft PowerPoint\n"
-mas install 462062816
+printf "AppStore: Installing The Unarchiver\n"
+mas install 425424353
+
+printf "AppStore: Installing VPN Surfshark - Private Web\n"
+mas install 1437809329
 
 
 printf "Installing offline apps..\n"
